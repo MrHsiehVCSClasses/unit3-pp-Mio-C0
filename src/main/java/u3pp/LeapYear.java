e2c0d69b9fcbdc21ec538c4befde8c20f6c516cb
Mio <mice384@vcs.email>
2021-10-18 14:33:15 -0700

    finished the project

--- src/main/java/u3pp/LeapYear.java
+++ src/main/java/u3pp/LeapYear.java
@@ -1,7 +1,7 @@
 package u3pp;
 
 /**
- * YOUR COMMENTs HERE
+ *
  * 
  * @author //Mio.C
  *
@@ -9,9 +9,33 @@ package u3pp;
 
 public class LeapYear {
 	/**
-	 * YOUR COMMENTS HERE
+	 * Building a program that will determine whether the given year is a leap year
 	 */
 	public static boolean isLeapYear(int year) {
-		/* YOUR CODE HERE */
+		if (year > 1582)
+		{
+
+			if (year % 100 != 0)
+			{
+
+				if (year % 4 == 0)
+				{
+					return true;
+				}
+				return false;
+			}
+			else if (year % 400 == 0)
+			{
+				return true;
+			}
+			else
+			{
+				return false;
+			}
+		}
+		else
+		{
+			return false;
+		}
+		}
 	}
-}
--- src/main/java/u3pp/Main.java
+++ src/main/java/u3pp/Main.java
@@ -3,25 +3,35 @@ package u3pp;
 import java.util.Scanner;
 
 /**
- * YOUR COMMENTS HERE
+ * Program takes year input from user and returns true or false if year is or isnt a leap year
  * 
- * @author YOUR NAME HERE
+ * @author Mio.C
  */
 
 class Main {
 
 	public static void main(String[] args) {
 
-		//YOUR CODE HERE
-
+		Scanner sc = new Scanner(System.in);
 		System.out.print("Enter a year (or enter STOP to end): ");
 		String input = sc.nextLine();
 
 		while (!input.toUpperCase().equals("STOP")) {
-			/* YOUR CODE HERE */
+			
+			int yearInput = Integer.parseInt(input);
+
+			if (LeapYear.isLeapYear(yearInput))
+			{
+				System.out.println(yearInput + " is a leap year");
+			}
+			else
+			{
+				System.out.println(yearInput + " is not a leap year");
+			}
+
 			System.out.print("Enter a year (or enter STOP to end): ");
-			//YOUR CODE HERE
+			input = sc.nextLine();
 		}
-		//YOUR CODE HERE
+		sc.close();
 	}
 }
